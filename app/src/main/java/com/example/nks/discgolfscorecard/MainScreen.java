ea180293736b49b50238f532f135e9c44e6015f6
nks <niklas.poussu3@luukku.com>
2017-09-07 13:32:22 +0300

    Holescore & individual hole counter working. Changed to Switch-Case to simplify code.

--- app/src/main/java/com/example/nks/discgolfscorecard/MainScreen.java
+++ app/src/main/java/com/example/nks/discgolfscorecard/MainScreen.java
@@ -1,6 +1,6 @@
 package com.example.nks.discgolfscorecard;
 
-import android.content.DialogInterface;
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,50 +9,65 @@ import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
-public class MainScreen extends AppCompatActivity {
+import static com.example.nks.discgolfscorecard.R.id.lastHole;
 
-    Button counterUp;
-    Button resetBtn;
-    Button holeCounterUp;
-    TextView scoreCount;
+public class MainScreen extends Activity implements View.OnClickListener {
+
+   TextView scoreCount;
     TextView numberOfHole;
-
+    private Button nextHole;
+    private Button lastHole;
+    private Button counter;
+    private Button reset;
+    int holeNumberInteger = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
 
-        holeCounterUp = (Button) findViewById(R.id.holeCounterUpID);
-        counterUp = (Button) findViewById(R.id.counterUpID);
-        resetBtn = (Button) findViewById(R.id.resetBtnID);
+        nextHole = (Button) findViewById(R.id.nextHole);
+        nextHole.setOnClickListener(MainScreen.this);
+        lastHole = (Button) findViewById(R.id.lastHole);
+        lastHole.setOnClickListener(MainScreen.this);
+        counter = (Button) findViewById(R.id.counterUpID);
+        counter.setOnClickListener(MainScreen.this);
+        reset = (Button) findViewById(R.id.resetBtnID);
+        reset.setOnClickListener(MainScreen.this);
         scoreCount = (TextView) findViewById(R.id.scoreCountID);
         numberOfHole = (TextView) findViewById(R.id.numberOfHoleID);
+        Button lastHole = (Button) findViewById(R.id.nextHole);
+        lastHole.setOnClickListener(this);
 
-        counterUp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String scoreValue = scoreCount.getText().toString();
-                int intScoreValue = Integer.parseInt(scoreValue);
-                intScoreValue++;
-                scoreCount.setText(String.valueOf(intScoreValue));
-            }
-        });
-        resetBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                scoreCount.setText("0");
-            }
-        });
-
-        holeCounterUp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String holeValue = numberOfHole.getText().toString();
-                int holeNumberInteger = Integer.parseInt(holeValue);
-                holeNumberInteger++;
-                numberOfHole.setText("Hole" + Integer.toString(holeNumberInteger));
-            }
-        });
     }
-}
+            public void onClick(View view) {
+                switch (view.getId()){
+                    case R.id.counterUpID:
+                        String scoreValue = scoreCount.getText().toString();
+                        int intScoreValue = Integer.parseInt(scoreValue);
+                        intScoreValue++;
+                        scoreCount.setText(String.valueOf(intScoreValue));
+                        break;
+                    case R.id.resetBtnID:
+                        scoreCount.setText("0");
+                        break;
+
+                    case R.id.nextHole:
+                        if (holeNumberInteger < 18) {
+                            holeNumberInteger++;
+                            numberOfHole.setText(String.valueOf(holeNumberInteger));
+                        }
+                            break;
+                    case R.id.lastHole:
+                        if (holeNumberInteger > 0) {
+                            holeNumberInteger--;
+                            numberOfHole.setText(String.valueOf(holeNumberInteger));
+                        }
+                            break;
+                }
+            }
+        }
+
+
+
+
