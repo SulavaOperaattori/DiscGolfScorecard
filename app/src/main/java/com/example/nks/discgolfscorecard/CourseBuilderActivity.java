0424cb11b7f643075190afda2fb767a34aed91b7
Samuli Piispanen <samuli.piispanen@student.tut.fi>
2017-09-11 12:10:08 +0300

    Removing unnecessary libraries and resolving library warnings.

--- app/src/main/java/com/example/nks/discgolfscorecard/CourseBuilderActivity.java
+++ app/src/main/java/com/example/nks/discgolfscorecard/CourseBuilderActivity.java
@@ -14,18 +14,15 @@ public class CourseBuilderActivity extends AppCompatActivity  {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_course_builder);
-        showAlertDialog();
-
-
-
         String [] holes  = {"Hole: 1", "Hole: 2", "Hole: 3", "Hole: 4", "Hole: 5", "Hole: 6", "Hole: 7", "Hole: 8", "Hole: 9", "Hole: 10", "Hole: 11", "Hole: 12",
                 "Hole: 13", "Hole: 14", "Hole: 15", "Hole: 16", "Hole: 17", "Hole: 18"};
         ListAdapter CourseBuilderList =  new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, holes );
         ListView list = (ListView) findViewById(R.id.list);
         list.setAdapter(CourseBuilderList);
+        showAlertDialog();
     }
-    private void showAlertDialog() {
 
+    private void showAlertDialog() {
         FragmentManager fm = getSupportFragmentManager();
         CourseDialog alertDialog = CourseDialog.newInstance("Some title");
         alertDialog.show(fm, "fragment_alert");
