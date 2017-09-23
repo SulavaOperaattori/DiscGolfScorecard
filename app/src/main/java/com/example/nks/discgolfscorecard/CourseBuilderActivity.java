c4f81f7172cb904785e2d55f20da72fed1f9e4bf
Samuli Piispanen <samuli.piispanen@student.tut.fi>
2017-09-23 15:08:48 +0300

    TODO: implement fragment and activity communication

--- app/src/main/java/com/example/nks/discgolfscorecard/CourseBuilderActivity.java
+++ app/src/main/java/com/example/nks/discgolfscorecard/CourseBuilderActivity.java
@@ -1,6 +1,7 @@
 package com.example.nks.discgolfscorecard;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -10,31 +11,32 @@ import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.util.Log;
 
 import java.util.ArrayList;
-
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
 
 public class CourseBuilderActivity extends AppCompatActivity {
 
     int lastShownItem;
     int duration = Toast.LENGTH_SHORT;
-    private ArrayList<Hole> holes;
+    Course newCourse;
     TextView cTextview;
+    private List<String> holes=new ArrayList<String>();
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         cTextview = (TextView) findViewById(R.id.seekBarText);
-
         showAlertDialog();
+        //setHoleInfo();
+
         setContentView(R.layout.activity_course_builder);
-        holes = new ArrayList<>();
-        final String[] holes = {"Hole: 1", "Hole: 2", "Hole: 3", "Hole: 4", "Hole: 5", "Hole: 6", "Hole: 7", "Hole: 8", "Hole: 9", "Hole: 10", "Hole: 11", "Hole: 12",
-                "Hole: 13", "Hole: 14", "Hole: 15", "Hole: 16", "Hole: 17", "Hole: 18"};
-
-
-
         ListAdapter CourseBuilderList = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, holes);
         ListView list = (ListView) findViewById(R.id.list);
         list.setAdapter(CourseBuilderList);
@@ -43,7 +45,7 @@ public class CourseBuilderActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 lastShownItem = i;
-                Toast.makeText(CourseBuilderActivity.this, holes[lastShownItem], duration).show();
+                Toast.makeText(CourseBuilderActivity.this, holes.get(lastShownItem), duration).show();
             }
         });
     }
@@ -52,7 +54,12 @@ public class CourseBuilderActivity extends AppCompatActivity {
         FragmentManager fm = getSupportFragmentManager();
         CourseDialog alertDialog = CourseDialog.newInstance("Create a new course");
         alertDialog.show(fm, "fragment_alert");
+        newCourse = alertDialog.getNewCourse();
+    }
 
-
+    public void setHoleInfo() {
+        for (int i=0; i < newCourse.getHoles(); i++) {
+            holes.add("Hole: "+(i+1));
+        }
     }
 }
